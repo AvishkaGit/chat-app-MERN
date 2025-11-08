688ffe1bcc89e1344f9c52d13f71c3956f5da491
Avishka madhuranga <131765154+AvishkaGit@users.noreply.github.com>
2025-11-08 21:53:08 +0530

    side bar update

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,10 +2,13 @@ import React, { useContext } from 'react'
 import assets, { userDummyData } from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
+import { ChatContext } from '../../context/ChatContext'
 
-const Sidebar = ({ selectedUser, setSelectedUser }) => {
+const Sidebar = () => {
 
-    const { logout } = useContext(AuthContext)
+    const { getUsers, users, selectedUser, setSelectedUser, unseenMessages, setUnseenMessages } = useContext(ChatContext);
+
+    const { logout, onlineUsers } = useContext(AuthContext);
 
     const navigate = useNavigate();
 
